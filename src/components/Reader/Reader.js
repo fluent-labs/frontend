4a5dbe00af0c661f5de4dadf1ec3ab96e1758a9a
Lucas Kjaero <lucas@lucaskjaero.com>
2020-12-18 23:26:51 -0800

    Bring over the app

---
+++ src/components/Reader/Reader.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+import LanguageInput from "./LanguageInput";
+import Vocabulary from "./Vocabulary";
+
+class Reader extends Component {
+  state = {
+    language: "",
+    text: "",
+    submitted: false
+  };
+
+  handleSubmit = ({ text, language }) => {
+    this.setState({
+      language: language,
+      submitted: true,
+      text: text
+    });
+  };
+
+  render = () => {
+    return (
+      <div>
+        <LanguageInput onSubmit={this.handleSubmit} />
+        <Vocabulary
+          language={this.state.language}
+          text={this.state.text}
+          submitted={this.state.submitted}
+        />
+      </div>
+    );
+  };
+}
+
+export default Reader;
