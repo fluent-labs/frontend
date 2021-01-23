07508ead01866ff0a84665baaa2fe7dbfb1ef976
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-23 12:37:36 -0800

    Vocabulary implemented with client

--- src/components/Reader/Vocabulary.tsx
+++ src/components/Reader/Vocabulary.tsx
@@ -1,54 +1,60 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 
 import { Card, Dimmer, Loader } from "semantic-ui-react";
 import Word from "./Word";
+import { ApiClient, WordDTO } from "../../client/api/ApiClient";
 
-type WordInfo = {
-  token: string;
-  language: string;
-  tag: string;
-  lemma: string;
-  definitions: Array<string>;
-  hsk?: number;
-  pinyin?: string;
-};
-
-type VocabularyProps = {
+interface VocabularyProps {
   language: string;
   text: string;
-  submitted: boolean;
 };
 
-const Vocabulary = ({ language, text, submitted }: VocabularyProps) => {
-  const data: Array<WordInfo> = [];
-  const loading = false;
-  const error = true;
+interface VocabularyState {
+  submissionState: SubmissionState;
+  data: Array<WordDTO>;
+}
 
-  if (!submitted) return <p>Submit text to see some vocabulary.</p>;
+enum SubmissionState {
+  PENDING,
+  LOADING,
+  SUCCESS,
+  FAILURE
+}
 
-  if (loading) {
-    return (
-      <Dimmer active>
-        <Loader />
-      </Dimmer>
-    );
+class Vocabulary extends Component<VocabularyProps, VocabularyState> {
+  state = { submissionState: SubmissionState.PENDING, data: [] };
+  client = new ApiClient();
+
+  submit = (language: string, text: string) => {
+    this.setState({ submissionState: SubmissionState.LOADING });
+    this.client.getWordsInDocument(language, text).then(result => {
+      this.setState({ submissionState: SubmissionState.SUCCESS, data: result});
+    }).catch(e => {
+      this.setState({ submissionState: SubmissionState.FAILURE });
+    });
   }
-  if (error) return <p>Error loading vocabulary.</p>;
 
-  return (
-    <Card.Group>
-      {data.map((word) => (
-        <Word key={word.token} {...word} />
-      ))}
-    </Card.Group>
-  );
-};
+  render = () => {
 
-Vocabulary.propTypes = {
-  language: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  submitted: PropTypes.bool.isRequired,
+    if (this.state.submissionState === SubmissionState.PENDING) return <p>Submit text to see some vocabulary.</p>;
+
+    if (this.state.submissionState === SubmissionState.LOADING) {
+      return (
+        <Dimmer active>
+          <Loader />
+        </Dimmer>
+      );
+    }
+    if (this.state.submissionState === SubmissionState.FAILURE) return <p>Error loading vocabulary.</p>;
+
+    return (
+      <Card.Group>
+        {this.state.data.map((word: WordDTO) => (
+          <Word key={word.token} language={this.props.language} word={word} />
+        ))}
+      </Card.Group>
+    );
+  };
 };
 
 export default Vocabulary;
