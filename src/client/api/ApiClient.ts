0bc19bb6a0cb5f68fec1b8b205e47caf0c9b4e06
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-23 10:37:53 -0800

    Fix linting

--- src/client/api/ApiClient.ts
+++ src/client/api/ApiClient.ts
@@ -4,30 +4,44 @@ export type Definition = {
   id: string;
   subdefinitions: Array<string>;
   tag: string;
-  examples ?: Array<string>;
-  simplified ?: string;
-  traditional ?: string;
-  pronunciation ?: string;
-  hsk ?: string;
-}
+  examples?: Array<string>;
+  simplified?: string;
+  traditional?: string;
+  pronunciation?: string;
+  hsk?: string;
+};
 
 export type Word = {
   token: string;
   tag: string;
   lemma: string;
   definitions: Array<Definition>;
-}
+};
 
 export class ApiClient {
-  hostname = (window.location.hostname === "localhost") ? "http://localhost:9000" : "https://api.foreignlanguagereader.com";
+  hostname =
+    window.location.hostname === "localhost"
+      ? "http://localhost:9000"
+      : "https://api.foreignlanguagereader.com";
 
-  getDefinition = async (language: string, token: string): Promise<Array<Definition>> => {
-    const response = await axios.get<Array<Definition>>(`${this.hostname}/v1/language/definition/${language}/${token}/`);
+  getDefinition = async (
+    language: string,
+    token: string
+  ): Promise<Array<Definition>> => {
+    const response = await axios.get<Array<Definition>>(
+      `${this.hostname}/v1/language/definition/${language}/${token}/`
+    );
     return response.data;
-  }
+  };
 
-  getWordsInDocument = async (language: string, document: string): Promise<Array<Word>> => {
-    const response = await axios.post<Array<Word>>(`${this.hostname}/v1/language/document/${language}/`, { text: document });
+  getWordsInDocument = async (
+    language: string,
+    document: string
+  ): Promise<Array<Word>> => {
+    const response = await axios.post<Array<Word>>(
+      `${this.hostname}/v1/language/document/${language}/`,
+      { text: document }
+    );
     return response.data;
-  }
+  };
 }
