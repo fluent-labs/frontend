d6f2905f89dbbd75dc264c517eb3b0ee4f073eb2
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-23 10:29:04 -0800

    Api client

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,4 @@ import "semantic-ui-css/semantic.min.css";
 
 import Reader from "./components/Reader/Reader";
 
-// Environment detection
-// let apiUrl = "https://api.foreignlanguagereader.com/api/";
-// if (window.location.hostname === "localhost") {
-//   apiUrl = "http://localhost:8000/api/";
-// }
-
 ReactDOM.render(<Reader />, document.getElementById("root"));
