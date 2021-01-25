b678b1400cbac040c208d9df0dd8b26f87586df1
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-24 21:20:09 -0800

    Label for word

--- src/components/Reader/Word.tsx
+++ src/components/Reader/Word.tsx
@@ -1,74 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-import { Button, Card, List } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 import { WordDTO } from "../../client/api/ApiClient";
 
-type WordProps = {
-  language: string;
+interface WordProps {
+  // onDeselected(): void;
+  // onSelected(): void;
+  selected: boolean;
   word: WordDTO;
-};
+}
 
-type WordDisplayInfo = {
-  header: string;
-  meta: string;
-  definitions: Array<string>;
-};
-
-const getWordInfo = (word: WordDTO): WordDisplayInfo => {
-  return {
-    header: `${word.token} (${word.lemma})`,
-    meta: word.tag,
-    definitions: word.definitions.flatMap(
-      (definition) => definition.subdefinitions
-    ),
-  };
-};
-
-const getChineseWordInfo = (word: WordDTO): WordDisplayInfo => {
-  const pinyin =
-    word.definitions.length > 0
-      ? ` (${word.definitions[0].pronunciation})`
-      : "";
-
-  return {
-    header: `${word.token}${pinyin}`,
-    meta: word.tag,
-    definitions: word.definitions.flatMap(
-      (definition) => definition.subdefinitions
-    ),
-  };
-};
-
-const Word = ({ language, word }: WordProps) => {
-  const wordInfo: WordDisplayInfo =
-    language === "CHINESE" ? getChineseWordInfo(word) : getWordInfo(word);
-
+export const Word = ({ word, selected }: WordProps) => {
+  const name = selected ? "delete" : "add";
   return (
-    <Card>
-      <Card.Content>
-        <Card.Header>{wordInfo.header}</Card.Header>
-        <Card.Meta>{wordInfo.meta}</Card.Meta>
-        <Card.Description>
-          <List bulleted>
-            {wordInfo.definitions.map((definition) => {
-              return <List.Header key={definition}>{definition}</List.Header>;
-            })}
-          </List>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button basic color="green">
-          Add to vocabulary
-        </Button>
-      </Card.Content>
-    </Card>
+    <Button icon labelPosition="right">
+      {word.token}
+      <Icon name={name} />
+    </Button>
   );
 };
-
-Word.propTypes = {
-  language: PropTypes.string.isRequired,
-  word: PropTypes.object.isRequired,
-};
-
-export default Word;
