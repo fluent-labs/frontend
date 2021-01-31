e85593139978f1c4ae1d4bbaeb03d187efc0fe7a
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-31 01:11:26 -0800

    Deal with loading

--- src/components/Reader/Vocabulary.tsx
+++ src/components/Reader/Vocabulary.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container, Header } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 
 import { WordSelector } from "./WordSelector";
 import { DefinitionDTO, WordDTO } from "../../client/api/ApiClient";
@@ -11,10 +11,15 @@ interface VocabularyProps {
   definitions: Map<String, Array<DefinitionDTO>>;
 }
 
-export const Vocabulary = ({ words }: VocabularyProps) => {
+export const Vocabulary = ({ text, words }: VocabularyProps) => {
+  const tokens = words.map((word) => word.token);
+
+  if (words.length == 0) {
+    return (<Container text>{text}</Container>);
+  }
+
   return (
     <Container text>
-      <Header as="h2">Words</Header>
       {words.map((word) => {
         return <WordSelector key={word.token} word={word} selected />;
       })}
