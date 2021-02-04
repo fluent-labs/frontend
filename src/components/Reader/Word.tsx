cb48e57234f71e5091614261ea02ceb4874362e4
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-03 23:28:15 -0800

    Popover for showing definitions

---
+++ src/components/Reader/Word.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { Card, Popover, Typography } from 'antd';
+import { WordDTO, DefinitionDTO } from "../../client/api/ApiClient";
+
+import "antd/lib/popover/style/index.css";
+
+const { Text } = Typography;
+
+interface WordProps {
+  key: string;
+  word: WordDTO;
+  definitions: Array<DefinitionDTO>;
+}
+
+export const Word = ({ word, definitions }: WordProps) => {
+  console.log(`${word.token}: ${definitions}`);
+
+  if (definitions.length > 0) {
+    const content = (
+      <Card title={definitions[0].pronunciation}>
+        <ul>
+          {definitions[0].subdefinitions.map((subdefinition: string) => <li>{subdefinition}</li>)}
+        </ul>
+      </Card>
+    );
+
+    return (
+      <React.Fragment>
+        <Popover content={content} title="Title">
+          <Text underline type="success">{word.token}</Text>
+        </Popover>
+        <Text> </Text>
+      </React.Fragment>
+    )
+  }
+
+  // No definition case, TODO find a good way to indicate it.
+  return (
+    <React.Fragment>
+      <Text underline type="secondary">{word.token}</Text>
+      <Text> </Text>
+    </React.Fragment>
+  )
+}
