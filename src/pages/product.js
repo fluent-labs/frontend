c5995de7481ab0c3249799f32c49bab15f03535c
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-17 23:59:27 -0800

    Page to demo the reader app

---
+++ src/pages/product.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
+import Navigation from "../components/common/navigation/navigation"
+import { Reader } from "../components/Reader/Reader"
+
+import Footer from "../components/sections/footer"
+import { Container } from "../components/global"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Product" />
+    <Container>
+        <Reader />
+    </Container>
+    <Footer />
+  </Layout>
+)
+
+export default IndexPage
