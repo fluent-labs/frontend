eaaca8fe9479381d46a00dbd75891fd39396b788
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-19 00:13:40 -0800

    Generate pages on site

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
