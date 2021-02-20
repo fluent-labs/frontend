084aeafb3b92e0a2046185c2a2f59b3df82a58ec
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-19 17:13:36 -0800

    Component for all pages

---
+++ src/components/common/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Layout from "./layout/layout";
+import SEO from "./layout/seo";
+import Navigation from "./navigation/navigation";
+
+import Footer from "../sections/footer";
+
+interface PageProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+const Page = ({ children, title }: PageProps) => (
+  <Layout>
+    <SEO title={title} />
+    <Navigation />
+    <>
+      {children}
+    </>
+    <Footer />
+  </Layout>
+);
+
+Page.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Page;
