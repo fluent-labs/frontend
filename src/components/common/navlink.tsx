4c8ad1c350f9096edc38ff0b3ed2e29102df9748
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-28 00:16:59 -0800

    Navigation link is a good candidate for being a component.

---
+++ src/components/common/navlink.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export interface NavLinkProps {
+  title: string;
+  href: string;
+  external: boolean;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+}
+
+const NavLink = ({ title, href, external, onClick = () => {} }: NavLinkProps) => {
+  if (external) {
+    return (
+      <a href={href} onClick={onClick}>
+        {title}
+      </a>
+    );
+  } else {
+    return (
+      <Link to={href} onClick={onClick}>
+        {title}
+      </Link>
+    );
+  }
+};
+
+export default NavLink;
