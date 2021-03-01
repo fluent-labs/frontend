35dad7c7683a0744b3a7e17aa937bbd200518345
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2021-03-01 10:09:01 -0800

    Enable service worker (#114)
    Enable service worker. This makes the service worker more resistant to bad network connections.

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,9 +65,7 @@ module.exports = {
         lang: "*",
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 };
 
