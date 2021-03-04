a909c68a8facbc062cfbd312c1f119f2edcd1838
Lucas Kjaero <lucas@lucaskjaero.com>
2021-03-04 00:29:10 -0800

    Do some tests

--- cypress/integration/index.test.js
+++ cypress/integration/index.test.js
@@ -2,5 +2,17 @@ describe("Check the home page", function () {
   beforeEach(function () {
     cy.visit("/");
   });
+
+  it("Renders the header", function () {
+    cy.get("h1").should("contain", "Read what you want");
+  });
+
+  it("Renders the benefits", function () {
+    cy.get("h4").should("contain", "Vocabulary");
+  });
+
+  it("Renders the get started", function () {
+    cy.get("button").should("contain", "Get early access");
+  });
 });
   
