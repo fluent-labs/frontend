4bca75680c2c3bca9877e30aa2fbeab6e6e10e2a
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2021-03-04 00:48:55 -0800

    Cypress smoke tests (#125)
    Do end-to-end browser tests on our main pages. This makes sure every page renders.

---
+++ cypress/integration/index.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable jest/expect-expect */
+
+describe("Check the home page", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+
+  it("Renders the header", function () {
+    cy.get("h1").should("contain", "Read what you want");
+  });
+
+  it("Renders the benefits", function () {
+    cy.get("h4").should("contain", "Vocabulary");
+  });
+
+  it("Renders the get started", function () {
+    cy.get("button").should("contain", "Get early access");
+  });
+});
