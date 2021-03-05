c12221bbbe85166990ae8978cb4041e6b0182887
Lucas Kjaero <lucas@lucaskjaero.com>
2021-03-04 23:01:38 -0800

    Write a test

---
+++ src/components/Reader/__tests__/word.spec.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import { Word } from "../word";
+
+describe("Word", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Word
+          key="test"
+          word={{
+            token: "test",
+            processedToken: "word",
+            tag: "noun",
+            lemma: "word",
+            definitions: [],
+          }}
+          definitions={[{ id: "test", subdefinitions: [], tag: "noun" }]}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
