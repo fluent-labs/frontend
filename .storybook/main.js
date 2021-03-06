6970c3a8b60e7940820c17d4367459d2092df171
Lucas Kjaero <lucas@lucaskjaero.com>
2021-03-05 23:26:51 -0800

    Format all the things

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,7 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  webpackFinal: async config => {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
     // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
@@ -24,7 +18,7 @@ module.exports = {
       require.resolve("babel-plugin-remove-graphql-queries"),
     ];
     // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
-    config.resolve.mainFields = ["browser", "module", "main"]
+    config.resolve.mainFields = ["browser", "module", "main"];
 
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
